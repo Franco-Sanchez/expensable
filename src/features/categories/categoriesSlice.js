e9cd95717a4a17fa9feb1f73803f7b601c538162
Franco <francorsr98@gmail.com>
2021-03-22 11:06:13 -0600

    Requests to the API were created

--- src/features/categories/categoriesSlice.js
+++ src/features/categories/categoriesSlice.js
@@ -1,14 +1,88 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { BASE_URI } from "../../app/config";
 
-const categoriesSlice = createSlice({
-  name: 'categories',
+export const fetchCategories = createAsyncThunk(
+  "categories/fetchCategories",
+  async ({ token }) => {
+    const response = await fetch(`${BASE_URI}/categories`, {
+      method: "GET",
+      headers: {
+        Authorization: `Token token=${token}`,
+      },
+    });
+
+    const data = await response.json();
+    if (!response.ok) {
+      throw new Error(JSON.stringify(data.errors));
+    }
+
+    return { categories: data };
+  }
+);
+
+export const fetchDeleteCategory = createAsyncThunk(
+  "categories/fetchDeleteCategories", async ({ token, id }) => {
+    const response = await fetch(`${BASE_URI}/categories/${id}`, {
+      method: 'DELETE',
+      headers: {
+        Authorization: `Token token=${token}`
+      }
+    })
+
+    const data = await response.json()
+    if(response.status === 204) {
+      return true;
+    } else {
+      throw new Error(JSON.stringify(data.errors)) // debería retornar un array
+    }
+  }
+)
+
+export const categoriesSlice = createSlice({
+  name: "categories",
   initialState: {
-    items: [],
-    state: 'idle',
-    error: null
+    expenses: [],
+    income: [],
+    // items: [],
+    status: "idle",
+    errors: null,
   },
-  reducers: {},
-  extraReducers: {}
-})
+  reducers: {
+    removeCategoryFromItems(state, action) {
+      state.items = state.items.filter(item => item.id !== action.payload.id);
+    }
+  },
+  extraReducers: {
+    [fetchCategories.pending]: (state) => {
+      state.status = "loading";
+    },
+    [fetchCategories.fulfilled]: (state, action) => {
+      state.status = "succeeded";
+      // state.items = action.payload.categories;
+      state.expenses = action.payload.categories.filter(
+        (category) => category.transaction_type === "expenses"
+      );
+      // save expenses in sessionStorage or localStorage
+      state.income = action.payload.categories.filter(
+        (category) => category.transaction_type === 'income'
+      );
+      // save income in sessionStorage or localStorage
+      state.errors = null;
+    },
+    [fetchCategories.rejected]: (state, action) => {
+      state.status = "failed";
+      state.errors = JSON.parse(action.error.message);
+    },
+    [fetchDeleteCategory.pending]: (state) => {
+      state.status = "loading";
+    },
+    [fetchDeleteCategory.rejected]: (state, action) => {
+      state.status = "failed";
+      state.errors = JSON.parse(action.error.message);
+    },
+  },
+});
+
+export const { removeCategoryFromItems } = categoriesSlice.actions;
 
 export default categoriesSlice.reducer;
--- src/features/session/sessionSlice.js
+++ src/features/session/sessionSlice.js
@@ -1,12 +1,67 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { BASE_URI } from '../../app/config';
+
+export const fetchLogin = createAsyncThunk('session/fetchLogin', async ({ formData }) => {
+  const response = await fetch(`${BASE_URI}/login`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(formData)
+  })
+
+  const data = await response.json();
+  if(!response.ok) {
+    throw new Error('Something was wrong');
+  }
+
+  return { token: data.token }
+})
+
+export const fetchLogout = createAsyncThunk('session/fecthLogout', async ({ token }) => {
+  const response = await fetch(`${BASE_URI}/logout`, {
+    method: 'DELETE',
+    headers: {
+      Authorization: `Token token=${token}`
+    }
+  })
+
+  if(response.status !== 204) {
+    throw new Error('Something was wrong');
+  }
+
+  return true;
+})
 
 const sessionSlice = createSlice({
   name: 'session',
   initialState: {
-    token: sessionStorage.getItem('token')
+    token: sessionStorage.getItem('token'),
+    status: 'idle',
+    error: null
   },
   reducers: {},
-  extraReducers: {}
+  extraReducers: {
+    [fetchLogin.pending]: (state) => {
+      state.status = 'pending';
+    },
+    [fetchLogin.fulfilled]: (state, action) => {
+      state.status = 'succeeded';
+      state.token = action.payload.token;
+      state.error = null;
+    },
+    [fetchLogin.rejected]: (state, action) => {
+      state.status = 'failed';
+      state.error = action.error.message;
+    },
+    [fetchLogout.pending]: (state) => {
+      state.status = 'pending';
+    },
+    [fetchLogout.rejected]: (state, action) => {
+      state.status = 'rejected';
+      state.error = action.error.message;
+    },
+  }
 })
 
 export default sessionSlice.reducer;

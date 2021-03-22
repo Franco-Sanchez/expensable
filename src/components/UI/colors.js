0023ca640e48c84eeedf23568a2533717015493f
Franco <francorsr98@gmail.com>
2021-03-22 11:18:18 -0600

    Global styles were added

---
+++ src/components/UI/colors.js
@@ -0,0 +1,10 @@
+const colors = {
+  gray1: '#333333',
+  gray2: '#4F4F4F',
+  gray4: '#BDBDBD',
+  blue1: '#2F80ED',
+  blue2: '#2D9CDB',
+  red: '#EB5757'
+}
+
+export default colors;
--- src/index.js
+++ src/index.js
@@ -4,10 +4,31 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './app/store';
 import { Provider } from 'react-redux';
+import { Global, css } from '@emotion/react';
+
+const globalStyles = css`
+  @import url('https://fonts.googleapis.com/css2?family=Inter&family=Roboto&display=swap');
+  
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    max-width: 1440px;
+    margin: 0 auto;
+  }
+
+  .root {
+    min-height: 100vh;
+  }
+`
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
+      <Global styles={globalStyles} />
       <App />
     </Provider>
   </React.StrictMode>,

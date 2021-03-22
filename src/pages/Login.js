be0cc29fca792362df9d8475be3c8f3d9d5e58d1
Franco <francorsr98@gmail.com>
2021-03-22 14:14:22 -0600

    Added styles to the Login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,15 +1,28 @@
 import { Content } from '../components/UI/texts/Content';
 import { Heading2 } from '../components/UI/texts/Headings';
 import LoginForm from '../features/session/LoginForm';
+import styled from '@emotion/styled';
 
 function Login() {
   return (
-    <div>
+    <StyledContainer>
       <Heading2>Welcome to Expensable</Heading2>
       <Content>Please login to start using the app</Content>
       <LoginForm />
-    </div>
+    </StyledContainer>
   )
 }
 
 export default Login;
+
+const StyledContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 32px;
+  padding: 32px 58px 0 58px;
+
+  & h2 {
+    text-align: center;
+  }
+`
